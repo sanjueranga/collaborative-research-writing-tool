23cefc85cbbab2bcdb9921aa8c375d1f63842ef7
sanjueranga <sanjueranga16@gmail.com>
2024-02-20 19:05:31 +0530

    user to admin panel

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import UserProfile,Interest,Country
 
-# Register your models here.
+
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ("id", "title", "user","time_stamp")
+
+class InterestAdmin(admin.ModelAdmin):
+    list_display = ("id", "label")
+    
+class CountryAdmin(admin.ModelAdmin):
+     list_display = ("id", "label")
+
+
+admin.site.register(UserProfile, UserProfileAdmin)
+admin.site.register(Interest, InterestAdmin)
+admin.site.register(Country, CountryAdmin)
+
