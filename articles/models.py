3f4f2935bf69ed6680bac00e952200161d298292
sanjueranga <sanjueranga16@gmail.com>
2024-02-20 18:59:40 +0530

    WIP

--- articles/models.py
+++ articles/models.py
@@ -5,6 +5,8 @@ from users.models import Interest
 
 class ArticleStatus(models.Model):
     label = models.CharField(max_length=100)
+    def __str__(self):
+        return self.label
 
 class Article(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE,related_name='articles_created')
@@ -18,26 +20,37 @@ class Article(models.Model):
     summary = models.TextField(max_length=500,blank=True,null=True)
     current_reviewer = models.ForeignKey(User,on_delete=models.SET_NULL,blank=True,null=True,related_name='articles_reviewing')
 
+    def __str__(self):
+        return self.title
+
 
 class ArticleComment(models.Model):
     user = models.ForeignKey(User,on_delete=models.CASCADE)
     content = models.TextField(max_length = 500, blank = True,null=True)
     article = models.ForeignKey(Article,on_delete=models.CASCADE)
+    def __str__(self):
+        return self.content
 
 
 class ReviewRequestStatus(models.Model):
     label = models.CharField(max_length=100)
+    def __str__(self):
+        return self.label
 
 class ReviewRequest(models.Model):
     reviewer =  models.ForeignKey(User,on_delete=models.SET_NULL,blank=True,null=True)
     article = models.ForeignKey(Article,on_delete=models.CASCADE)
     status = models.ForeignKey(ReviewRequestStatus,on_delete=models.SET_NULL,blank=True,null=True)
     declined_reason = models.TextField(max_length=500,blank=True,null=True)
+    def __str__(self):
+        return f"{self.article.title} : status {self.status}"
 
 
 class Suggestion(models.Model):
     review_request= models.ForeignKey(ReviewRequest,on_delete=models.CASCADE)
     title = models.CharField(max_length=100)
     content = models.TextField(max_length=1000,blank=True,null=True)
+    def __str__(self):
+        return self.title
 
     
