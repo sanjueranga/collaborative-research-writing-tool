9660cd879be1e336a5e13c4258b1ac78b42a0a2c
sanjueranga <sanjueranga16@gmail.com>
2024-02-21 13:57:22 +0530

    change: suggestion model

--- articles/models.py
+++ articles/models.py
@@ -41,15 +41,15 @@ class ReviewRequest(models.Model):
     reviewer =  models.ForeignKey(User,on_delete=models.CASCADE,related_name='review_request')
     article = models.ForeignKey(Article,on_delete=models.CASCADE)
     status = models.ForeignKey(ReviewRequestStatus,on_delete=models.PROTECT)
-    declined_reason = models.TextField(max_length=500,blank=True,null=True)
+    declined_reason = models.CharField(max_length=500,blank=True,null=True)
     def __str__(self):
         return f"{self.article.title} : status {self.status}"
 
 
 class Suggestion(models.Model):
-    review_request= models.ForeignKey(ReviewRequest,on_delete=models.CASCADE)
+    review_request= models.OneToOneField(ReviewRequest,on_delete=models.CASCADE,unique=True)
     title = models.CharField(max_length=100)
-    content = models.TextField(max_length=1000,blank=True,null=True)
+    content = models.TextField(blank=True,null=True)
     def __str__(self):
         return self.title
 
