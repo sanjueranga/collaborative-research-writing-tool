b6dac7a5405bb6ac54b3897242691c79e813c1f2
sanjueranga <sanjueranga16@gmail.com>
2024-02-21 12:45:46 +0530

    change: contry label unique

--- users/models.py
+++ users/models.py
@@ -4,7 +4,7 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
         
 class Country(models.Model):
-    label = models.CharField(max_length=100)
+    label = models.CharField(max_length=100,unique=True)
     def __str__(self):
         return self.label
 
