e2ab43e6dd621a11a03b5fb81cdc6f826d524f10
sanjueranga <sanjueranga16@gmail.com>
2024-02-21 12:36:22 +0530

    fix: make country field required

--- users/models.py
+++ users/models.py
@@ -18,7 +18,7 @@ class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     company_name = models.CharField(max_length=200, null=True, blank=True)
     profile_picture = models.CharField(max_length=100, blank=True, null=True)
-    country = models.ForeignKey(Country, on_delete=models.SET_NULL, blank=True, null=True)
+    country = models.ForeignKey(Country, on_delete=models.PROTECT, blank=False)
     job_title = models.CharField(max_length=100)
     experience = models.ForeignKey(Experience, on_delete=models.SET_NULL, blank=True, null=True)  # Changed "Experience" to "experience" to follow Python conventions
     website = models.URLField(max_length=200)
