8a19fc9ca5cb1b469ab12b4742227fd3afacf29b
sanjueranga <sanjueranga16@gmail.com>
2024-02-21 14:06:50 +0530

    userprofile

--- articles/models.py
+++ articles/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-from users.models import Interest
+from userprofile.models import Interest
 
 
 class ArticleStatus(models.Model):
@@ -25,8 +25,8 @@ class Article(models.Model):
 
 
 class ArticleComment(models.Model):
-    user = models.ForeignKey(User,on_delete=models.CASCADE)
-    content = models.CharField(max_length = 500, related_name='comments')
+    user = models.ForeignKey(User,on_delete=models.CASCADE,related_name="comments")
+    content = models.CharField(max_length = 500)
     article = models.ForeignKey(Article,on_delete=models.CASCADE)
     def __str__(self):
         return self.content
--- research_writing_tool/settings.py
+++ research_writing_tool/settings.py
@@ -39,8 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'users',
-    'articles'
+    # 'articles'
 ]
 
 MIDDLEWARE = [
